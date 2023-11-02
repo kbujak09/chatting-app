abc718f475f6b97b75ecc48d2c48a8d1b36d8e65
Kacper Bujak <kaccpper2@gmail.com>
2023-11-02 14:22:13 +0100

    miniatures have fetched data

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,41 @@
 import Miniature from '../miniature/Miniature';
 import styles from './content.module.css';
+import { useState, useEffect } from 'react';
 
 const Content = () => {
 
+  const [conversations, setConversations] = useState();
+
+  const fetchConversations = async () => {
+    try {
+      const res = await fetch(`http://192.168.0.15:5000/api/conversations?userId=${localStorage.id}`);
+
+      if (!res.ok) {
+        console.error(`Failed to fetch conversations: ${res.status}`);
+      }
+
+      const data = await res.json();
+      console.log(data)
+
+      if (data) {
+        await setConversations(data);
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchConversations();
+    console.log(conversations)
+  }, []);
+
   return (
     <div className={styles.content}>
-      <Miniature />
+      {conversations && conversations.map(item => {
+        return <Miniature data={item}/>
+      })}
     </div>
   )
 }
--- src/components/miniature/Miniature.jsx
+++ src/components/miniature/Miniature.jsx
@@ -1,16 +1,43 @@
 import styles from './miniature.module.css';
 import avatar from '../../assets/avatar.png';
 
-const Miniature = () => {
+const Miniature = ({data}) => {
+
+  const getUsername = (arr) => {
+    const user = arr.filter(item => item._id !== localStorage.id);
+    return user[0].username;
+  }
+  
+  function convertTime(dateString) {
+    const inputDate = new Date(dateString);
+    const currentDate = new Date();
+    const timeDifference = currentDate - inputDate;
+    const hoursAgo = timeDifference / (1000 * 60 * 60);
+  
+    let formattedDate;
+  
+    if (hoursAgo < 24) {
+      formattedDate = inputDate.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
+    } else if (hoursAgo < 24 * 365.25) {
+      formattedDate = inputDate.toLocaleString('en-US', { day: 'numeric' });
+    } else {
+      formattedDate = Math.floor(hoursAgo / 24 / 365.25) + ' years ago';
+    }
+  
+    return formattedDate;
+  }
+
+  const message = data.messages[0];
+ 
   return (
     <div className={styles.miniature}>
       <div className={styles.avatar}><img src={avatar} alt='avatar' /></div>
       <div className={styles.info}>
-        <div className={styles.name}>Kacper Bujak</div>
+        <div className={styles.name}>{getUsername(data.members)}</div>
         <div className={styles.container}>
-          <div className={styles.text}>Hello world!</div>
+          <div className={styles.text}>{message.from._id === localStorage.id ? `You: ${message.text.substring(0, 30)}` : message.text.substring(0, 30)}</div>
           <span className={styles.dot}>•</span>
-          <div className={styles.date}>14:25</div>
+          <div className={styles.date}>{convertTime(message.createdAt)}</div>
         </div>
       </div>
     </div>
